8b72b1d21bfb06a259d5ab7ba52c08af02019f9e
Hector Velasco <hvelasco0@ucol.mx>
2022-05-14 14:08:27 -0500

    avance msjs

--- src/screens/MenuChat.js
+++ src/screens/MenuChat.js
@@ -1,14 +1,39 @@
-import React,{} from "react";
+import React,{useEffect} from "react";
 import {StyleSheet, View,Text,Button,ScrollView,TouchableOpacity} from "react-native";
-import { getFirestore } from "firebase/firestore";
-import { doc, setDoc,set,collection } from "firebase/firestore";
+import { getFirestore, onSnapshot } from "firebase/firestore";
+import { doc, setDoc,set,collection,getDocs,query } from "firebase/firestore";
 import { firebaseConfig } from "../utils/firebase";
 
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword} from 'firebase/auth';
 import { initializeApp } from 'firebase/app';
 
+import { database } from "../../config/firebase";
+
+import { getDatabase, ref, push, onValue} from "firebase/database"; 
 
 export default function MenuChat(){
+
+    const app = initializeApp(firebaseConfig);
+    //const database = getDatabase(app)
+
+
+    const collRef =  collection(database,'salas');
+    //const docSnap = getDoc(docRef);
+    const q = query(collRef);
+
+    const unsub = onSnapshot(q, (querySnapshot) =>{
+        querySnapshot.forEach((doc) =>{
+            console.log(doc.data(), "---", doc.id);
+        })
+    } );
+
+
+    const querySnapshot = getDocs(collRef);
+    console.log(unsub);
+
+    
+
+    
 
     const crearSala = () =>{
 
@@ -19,7 +44,7 @@ export default function MenuChat(){
         
         const document = doc(db,"salas","user");
         const data = {
-            "owner" : "amarillo",   
+            "owner" : "amareloo",   
         }
         setDoc(document,data)
         .then(() => console.log("Registrado"))
